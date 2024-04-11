76bc2a1369a3fa09fb1439dd7f85e6ef6b0b8f43
unknown <gautampanchal99204@gmail.com>
2024-04-12 01:14:15 +0530

    ui for listing created

--- server/models/listing.model.js
+++ server/models/listing.model.js
@@ -10,36 +10,12 @@ const listingSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    address: {
-      type: String,
-      required: true,
-    },
     regularPrice: {
       type: Number,
       required: true,
     },
     discountPrice: {
       type: Number,
-      required: true,
-    },
-    bathrooms: {
-      type: Number,
-      required: true,
-    },
-    bedrooms: {
-      type: Number,
-      required: true,
-    },
-    furnished: {
-      type: Boolean,
-      required: true,
-    },
-    parking: {
-      type: Boolean,
-      required: true,
-    },
-    type: {
-      type: String,
       required: true,
     },
     offer: {
